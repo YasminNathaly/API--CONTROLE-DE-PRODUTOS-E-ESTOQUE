b018a75369adbe1ca0a5f210747d208ed35b9ddf
Yasmin Cavalcanti <Ynathaly58@gmail.com>
2025-10-20 14:57:02 -0300

    nova alteração em API

--- Back_end/api.py
+++ Back_end/api.py
@@ -9,13 +9,13 @@ app = FastAPI(title="API Controle de Produtos e Estoque")
 # Adicionar um novo produto
 @app.post("/produtos")
 def criar_produto(nome: str, categoria: str = "", preco: float = 0, quantidade: int = 0):
-    resultado = funcoes.adicionar_produto(nome, categoria, preco, quantidade)
+    resultado = funcao.adicionar_produto(nome, categoria, preco, quantidade)
     return resultado
 
 # Listar todos os produtos
 @app.get("/produtos")
 def listar_produtos():
-    produtos = funcoes.listar_produtos()
+    produtos = funcao.listar_produtos()
     return {"produtos": produtos}
 
 # Atualizar produto
@@ -27,17 +27,17 @@ def atualizar_produto(
     preco: float = None,
     quantidade: int = None
 ):
-    resultado = funcoes.atualizar_produto(id, nome, categoria, preco, quantidade)
+    resultado = funcao.atualizar_produto(id, nome, categoria, preco, quantidade)
     return resultado
 
 # Excluir produto
 @app.delete("/produtos/{id}")
 def excluir_produto(id: int):
-    resultado = funcoes.excluir_produto(id)
+    resultado = funcao.excluir_produto(id)
     return resultado
 
 # Valor total do estoque
 @app.get("/produtos/valor_total")
 def valor_total_estoque():
-    total = funcoes.valor_total_estoque()
+    total = funcao.valor_total_estoque()
     return total
