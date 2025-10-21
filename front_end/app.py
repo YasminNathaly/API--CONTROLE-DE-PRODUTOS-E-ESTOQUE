9122ceaca600cbc44451e826875036cb966426b0
Yasmin Cavalcanti <Ynathaly58@gmail.com>
2025-10-21 08:55:03 -0300

    ATUALIZANDO APP

--- front_end/app.py
+++ front_end/app.py
@@ -4,9 +4,9 @@ import requests
 # Rodar o Streamlit:
 # python -m streamlit run app.py
 
-# URL da API do FastAPI
-API_URL = "http://127.0.0.1:8000"
+API_URL = "http://127.0.0.1:8000"  # Endereço da API
 
+response = requests.get(f"{API_URL}/produtos")  # Chama o endpoint de produtos
 st.set_page_config(page_title="Controle de Produtos", page_icon="📦")
 st.title("📦 Sistema de Controle de Produtos e Estoque")
 
